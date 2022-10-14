71f8012dc27ee1dc9c93264de4fe09a3519be2d3
Diego Oliveira de Araujo <doasalah@gmail.com>
2022-10-13 21:31:07 -0300

    adjust

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,15 +1,10 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { Envelope } from "phosphor-react";
-import {
-  TextInput,
-  TextInputInputProps,
-  TextInputRootProps,
-} from "./TextInput";
+import { TextInput, TextInputRootProps } from "./TextInput";
 
 export default {
   title: "Components/TextInput",
   component: TextInput.Root,
-  // subcomponents: [TextInput.Input],
   args: {
     children: [
       <TextInput.Icon>
